15234b1cb89f2a2d38fb55970bfabd948c4fbe48
JorgeCantu247 <jorge.cantu@atomic32.com>
2022-12-05 18:06:52 -0600

    another test

--- template/api/error/controllers/error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * error controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::error.error');
--- template/api/error/services/error.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * error service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::error.error');
